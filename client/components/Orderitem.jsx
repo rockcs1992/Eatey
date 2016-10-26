044f209341826e631789c2550e90014b42c63916
rockcs1992 <rockcs@foxmail.com>
2016-10-26 16:48:33 -0400

    remove node modules

--- client/components/Orderitem.jsx
+++ client/components/Orderitem.jsx
@@ -3,13 +3,28 @@ import React from 'react';
 export default class Orderitem extends React.Component {
 	constructor(props) {
         super(props);
-      
+      	this.state = {
+      		counter : 0
+      	};
     }
+
+    componentDidMount(){
+    	this.timer = setInterval(this.tick.bind(this),1000);
+	}
+
+	tick(){
+		this.setState({counter:++this.state.counter});
+		console.log(this.state.counter); 
+	}
+
+	componentWillUnmount(){
+		clearInterval(this.timer);
+	}
 
    
 
     render() {
-    	var order = this.props.order;
+    	const order = this.props.order;
         return (
             <tr>
             	<td>{order.restaurant}</td>
